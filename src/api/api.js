f9d2be530f306cb29a4aabe44a75c6f2734b8fe2
Ben Obee <benobee@gmail.com>
2018-06-06 11:44:27 -0700

    remove blog

--- src/api/api.js
+++ src/api/api.js
@@ -2,10 +2,6 @@ import controller from "../core/controller";
 import scrollControl from "../modules/scrollControl";
 
 const api = () => {
-    controller.on("blog", (el) => {
-        scrollControl.init(el);
-        console.log("blog page", el);
-    });
     controller.on("navbar", (el) => {
         scrollControl.init(el);
         console.log("navbar el", el);
